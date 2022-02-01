42a089cd72ea6fa8ffd822c1cf5a9f1bf28a849a
sasja koning <sasjakoning@hotmail.com>
2022-02-01 16:23:34 +0100

    svg edits

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // scroll drawing : https://css-tricks.com/scroll-drawing/
 
+let root = document.documentElement;
+
+var path = document.querySelector("#path1");
+console.log(path);
+
+var pathLength = path.getTotalLength();
+
+console.log(pathLength);
+
+// path.style.strokeDasharray = pathLength + "" + pathLength;
+
+root.style.setProperty("--stroke-dasharray", pathLength + " " + pathLength);
+
+// path.style.strokeDashoffset = pathLength;
+
+root.style.setProperty("--stroke-dashoffset", pathLength - 200);
+
 setTimeout(() => {
-  let root = document.documentElement;
-
-  var path = document.querySelector("#path1");
-  console.log(path);
-
-  var pathLength = path.getTotalLength();
-
-  console.log(pathLength);
-
-  // path.style.strokeDasharray = pathLength + "" + pathLength;
-
-  root.style.setProperty("--stroke-dasharray", pathLength + " " + pathLength);
-
-  // path.style.strokeDashoffset = pathLength;
-
-  root.style.setProperty("--stroke-dashoffset", pathLength - 200);
-
   window.addEventListener("scroll", function (e) {
     var scrollPercentage =
       (document.documentElement.scrollTop + document.body.scrollTop) /
